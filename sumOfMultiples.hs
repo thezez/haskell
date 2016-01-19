a591e3fdb6d54bf4ee1a73a95ac077379d3a9ffa
thezez <thezez@gmail.com>
2016-01-19 17:54:52 +1300

    Update sumOfMultiples.hs
    Added min

--- sumOfMultiples.hs
+++ sumOfMultiples.hs
@@ -1,5 +1,5 @@
---Usage: sumOfMultiples maxValue multiple1 multiple2
+--Usage: sumOfMultiples minValue maxValue multiple1 multiple2
 -- e.g. sumOfMultiples 10 2 9 will return the sum of all multiples of either 2 or 9 up to and including 10. (39)
 
-sumOfMultiples::Int->Int->Int->Int
-sumOfMultiples max m1 m2 = sum[x | x <- [1..max], x `mod` m1 == 0 || x `mod` m2 == 0]
+sumOfMultiples::Int->Int->Int->Int->Int
+sumOfMultiples min max m1 m2 = sum[x | x <- [min..max], x `mod` m1 == 0 || x `mod` m2 == 0]
